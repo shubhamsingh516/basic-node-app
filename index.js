f6b1bbbdf158f78405ef890649fe590f3aad584e
shubham singh <bakwaasj@gmail.com>
2026-05-06 17:56:07 +0530

    add db query

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
 
 
@@ -10,18 +14,49 @@ app.get('/', (req, res) => {
 app.use(express.json());
 
 
-app.post('/data', (req, res) => {
+await mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+
+const dataSchema = new mongoose.Schema({
+    data: String
+});
+
+const Data = mongoose.model('Data', dataSchema);
+
+
+
+
+
+
+app.post('/data', async (req, res) => {
     const { data } = req.body;
     console.log('Received data:', data);
     const capitalizedData = data.toUpperCase();
 
+    const saved = await Data.create({ data: capitalizedData }).then((savedData) => {
+        console.log('Data saved to MongoDB:', savedData);
+        return savedData;
+    }).catch((err) => {
+        console.error('Error saving data to MongoDB:', err);
+        return null;
+    })
+
     res.json({
-        message: 'Data received and processed successfully',
+        message: 'Data received and processed and saved successfully',
         originalData: data,
-        processedData: capitalizedData
+        processedData: capitalizedData,
+        savedData: saved
 
     }).status(201);
 });
+
+
+
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
